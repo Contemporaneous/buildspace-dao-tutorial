701fad7c26ff9be631bcf8c9bafb70c816d41e35
Rhian McClelland <rhianmcclelland193@hotmail.com>
2022-02-18 18:14:26 +1000

    Add Connect Wallet Button

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ const App = () => {
         return (
         <div className="landing">
             <h1>Welcome to the Dope Goat DAO</h1>
+            <button onClick={() => connectWallet("injected")} className="btn-hero">
+                Connect your wallet
+            </button>
         </div>
         );
     };

10718de6ee90d5976096902c88293d6115982b5e
Rhian McClelland <rhianmcclelland193@hotmail.com>
2022-02-18 18:04:43 +1000

    Add wallet connection and ThirdWeb layer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { useEffect, useMemo, useState } from "react";
+
+// import thirdweb
+import { useWeb3 } from "@3rdweb/hooks";
+
 const App = () => {
+    const { connectWallet, address, error, provider } = useWeb3();
+    console.log("👋 Address:", address)
+    
+    if (!address) {
+        return (
+        <div className="landing">
+            <h1>Welcome to the Dope Goat DAO</h1>
+        </div>
+        );
+    };
+
     return (
-      <div className="landing">
-        <h1>Welcome to My DAO</h1>
-      </div>
-    );
-  };
-  
+        <div className="landing">
+          <h1>👀 wallet connected, now what!</h1>
+        </div>);
+    };
+    
   export default App;
   

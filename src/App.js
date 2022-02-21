1313750c5f0d5f51214f824bc3d7280d2a9900fc
Rhian McClelland <rhianmcclelland193@hotmail.com>
2022-02-22 06:15:45 +1000

    Update Member Landing Page with Balances

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { useEffect, useMemo, useState } from "react";
 import { ThirdwebSDK } from "@3rdweb/sdk";
-
-// import thirdweb
 import { useWeb3 } from "@3rdweb/hooks";
+import { ethers } from "ethers";
 
 //Initiate sdk on rinkeby
 const sdk = new ThirdwebSDK("rinkeby");
 
 const bundleDropModule = sdk.getBundleDropModule(
     "0xcc9CD9379239903D3E63bF7734bF7a6b45cd636E",
+);
+
+const tokenModule = sdk.getTokenModule(
+  "0x5670fBa87751DE88e6c852aEb6e9d2262F9BCCfC"
 );
 
 const App = () => {
@@ -21,9 +24,57 @@ const App = () => {
     // State variables
     const [hasClaimedNFT, setHasClaimedNFT] = useState(false);
     const [isClaiming, setIsClaiming] = useState(false);
+    const [memberTokenAmounts, setMemberTokenAmounts] = useState({});
+    const [memberAddresses, setMemberAddresses] = useState([]);
 
+    const shortenAddress = (str) => {
+      return str.substring(0, 6) + "..." + str.substring(str.length - 4);
+    };
 
     //useEffects
+    useEffect(async () => {
+      if (!hasClaimedNFT) {
+        return;
+      }
+      
+      // Just like we did in the 7-airdrop-token.js file! Grab the users who hold our NFT
+      // with tokenId 0.
+      try {
+        const memberAddresses = await bundleDropModule.getAllClaimerAddresses("0");
+        setMemberAddresses(memberAddresses);
+        console.log("🚀 Members addresses", memberAddresses);
+      } catch (error) {
+        console.error("failed to get member list", error);
+      }
+    }, [hasClaimedNFT]);
+
+    useEffect(async () => {
+      if (!hasClaimedNFT) {
+        return;
+      }
+    
+      // Grab all the balances.
+      try {
+        const amounts = await tokenModule.getAllHolderBalances();
+        setMemberTokenAmounts(amounts);
+        console.log("👜 Amounts", amounts);
+      } catch (error) {
+        console.error("failed to get token amounts", error);
+      }
+    }, [hasClaimedNFT]);
+
+    const memberList = useMemo(() => {
+      return memberAddresses.map((address) => {
+        return {
+          address,
+          tokenAmount: ethers.utils.formatUnits(
+            memberTokenAmounts[address] || 0,
+            18,
+          ),
+        };
+      });
+    }, [memberAddresses, memberTokenAmounts]);
+
     useEffect(() => {
         sdk.setProviderOrSigner(signer);
       }, [signer]);
@@ -68,9 +119,33 @@ const App = () => {
         <div className="member-page">
           <h1>Dope Goat DAO Member Page</h1>
           <p>Congratulations on being a member</p>
+          <div>
+            <div>
+              <h2>Member List</h2>
+              <table className="card">
+                <thead>
+                  <tr>
+                    <th>Address</th>
+                    <th>Token Amount</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {memberList.map((member) => {
+                    return (
+                      <tr key={member.address}>
+                        <td>{shortenAddress(member.address)}</td>
+                        <td>{member.tokenAmount}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
       );
     };
+    
 
     const mintNft = async () => {
         setIsClaiming(true);

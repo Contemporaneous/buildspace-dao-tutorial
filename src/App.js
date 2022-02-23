30f1fd85fba153bac26190b9c2aad0e557a2b2c0
Rhian McClelland <rhianmcclelland193@hotmail.com>
2022-02-23 19:55:41 +1000

    Add Chain Check

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useMemo, useState } from "react";
 import { ThirdwebSDK } from "@3rdweb/sdk";
 import { useWeb3 } from "@3rdweb/hooks";
 import { ethers } from "ethers";
+import { UnsupportedChainIdError } from "@web3-react/core";
 
 //Initiate sdk on rinkeby
 const sdk = new ThirdwebSDK("rinkeby");
@@ -150,6 +151,18 @@ const App = () => {
         console.error("Failed to check if wallet has voted", error);
       }
     }, [hasClaimedNFT, proposals, address]);
+
+    if (error instanceof UnsupportedChainIdError ) {
+      return (
+        <div className="unsupported-network">
+          <h2>Please connect to Rinkeby</h2>
+          <p>
+            This dapp only works on the Rinkeby network, please switch networks
+            in your connected wallet.
+          </p>
+        </div>
+      );
+    }
     
     if (!address) {
         return (
